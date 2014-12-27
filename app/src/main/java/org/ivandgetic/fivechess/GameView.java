8cd26e97249e5987fe1dad42f9dfbaa498d97eb2
ivandgetic <ivandgetic@gmail.com>
2014-12-27 18:23:30 +0800

    Use Surface instead of View.

--- app/src/main/java/org/ivandgetic/fivechess/GameView.java
+++ app/src/main/java/org/ivandgetic/fivechess/GameView.java
@@ -6,7 +6,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -14,25 +15,21 @@ import java.io.IOException;
 /**
  * Created by ivandgetic on 14/7/9.
  */
-public class GameView extends View {
-    Paint paint = null;
-    int startX = 0;
-    int startY = 0;
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     public static int CHESS_BLACK = 1;
     public static int CHESS_WHITE = 2;
-    int GRID_WIDTH = GameConfig.getWIDTH() / GameConfig.getPOINT_NUM();
     public static int[][] chess = new int[GameConfig.getPOINT_NUM()][GameConfig.getPOINT_NUM()];
     public static GameView gameView = null;
     public static boolean turn = false;
     public static boolean gameEnd = false;
     public static int myColor = 1;
-    static String line=null;
-    static String lastx=null;
-    static String lasty=null;
-
-    public static GameView getGameView() {
-        return gameView;
-    }
+    static String line = null;
+    static String lastx = null;
+    static String lasty = null;
+    Paint paint = null;
+    int startX = 0;
+    int startY = 0;
+    int GRID_WIDTH = GameConfig.getWIDTH() / GameConfig.getPOINT_NUM();
 
     public GameView(Context context) {
         super(context);
@@ -49,97 +46,8 @@ public class GameView extends View {
         InitGameView();
     }
 
-    public void InitGameView() {
-        gameView = this;
-        paint = new Paint();
-        paint.setAntiAlias(true);
-        startX = GRID_WIDTH / 2;
-        startY = GRID_WIDTH / 2;
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawColor(0xffe69310);
-        paint.setColor(Color.BLACK);//设置画笔为黑色
-        paint.setStrokeWidth(2.5f);//设置画笔宽度
-        for (int i = 0; i < GameConfig.getPOINT_NUM(); i++) {
-            canvas.drawLine(startX, startY + i * GRID_WIDTH, startX + GameConfig.getGRID_NUM()
-                    * GRID_WIDTH, startY + i * GRID_WIDTH, paint);//画横线
-            canvas.drawLine(startX + i * GRID_WIDTH, startY, startX + i
-                    * GRID_WIDTH, startY + GameConfig.getGRID_NUM() * GRID_WIDTH, paint);//画竖线
-        }
-        for (int i = 0; i < GameConfig.getPOINT_NUM(); i++) {
-            for (int j = 0; j < GameConfig.getPOINT_NUM(); j++) {
-                if (chess[i][j] == CHESS_BLACK) {
-                    paint.setColor(Color.BLACK);
-                    canvas.drawCircle(startX + i * GRID_WIDTH, startY + j * GRID_WIDTH, GRID_WIDTH / 2 - GRID_WIDTH * 0.05f, paint);
-                }
-                if (chess[i][j] == CHESS_WHITE) {
-                    paint.setColor(Color.WHITE);
-                    canvas.drawCircle(startX + i * GRID_WIDTH, startY + j * GRID_WIDTH, GRID_WIDTH / 2 - GRID_WIDTH * 0.05f, paint);
-                }
-            }
-        }
-        if (line!=null){
-            String[] separateLine=line.split(":",4);
-            paint.setColor(0xffff0000);
-            canvas.drawLine(startX+Integer.parseInt(separateLine[0])*GRID_WIDTH,startY+Integer.parseInt(separateLine[1])*GRID_WIDTH,startX+Integer.parseInt(separateLine[2])*GRID_WIDTH,startY+Integer.parseInt(separateLine[3])*GRID_WIDTH,paint);
-        }
-        if (!gameEnd){
-            if (lastx!=null&&lasty!=null){
-                paint.setColor(0xffff0000);
-                canvas.drawLine(startX+Integer.parseInt(lastx)*GRID_WIDTH-GRID_WIDTH*0.2f,startY+Integer.parseInt(lasty)*GRID_WIDTH,startX+Integer.parseInt(lastx)*GRID_WIDTH+GRID_WIDTH*0.2f,startY+Integer.parseInt(lasty)*GRID_WIDTH,paint);
-                canvas.drawLine(startX+Integer.parseInt(lastx)*GRID_WIDTH,startY+Integer.parseInt(lasty)*GRID_WIDTH-GRID_WIDTH*0.2f,startX+Integer.parseInt(lastx)*GRID_WIDTH,startY+Integer.parseInt(lasty)*GRID_WIDTH+GRID_WIDTH*0.2f,paint);
-            }
-        }
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        float touchX = event.getX();
-        float touchY = event.getY();
-        if (((((touchX - startX) % GRID_WIDTH) < GRID_WIDTH * 0.4) || (((touchX - startX) % GRID_WIDTH) > GRID_WIDTH * 0.6))
-                && ((((touchY - startY) % GRID_WIDTH) < GRID_WIDTH * 0.4) || (((touchY - startY) % GRID_WIDTH) > GRID_WIDTH * 0.6))) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    int indexX = Math.round((touchX - startX) / GRID_WIDTH);
-                    int indexY = Math.round((touchY - startY) / GRID_WIDTH);
-                    if (turn && chess[indexX][indexY] == 0 && !gameEnd) {
-                        chess[indexX][indexY] = myColor;
-                        try {
-                            MyService.getMyService().out.writeUTF("play:place:" + GameConfig.getPartner() + ":" + indexX + ":" + indexY + ":" + myColor);
-                            lastx=String.valueOf(indexX);
-                            lasty=String.valueOf(indexY);
-                            turn = false;
-                            check();
-                            if (!GameView.gameEnd) {
-                                if (GameViewFragment.textViewPlayer1Turn != null) {
-                                    GameViewFragment.textViewPlayer1Turn.post(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            GameViewFragment.textViewPlayer1Turn.setText("");
-                                        }
-                                    });
-                                }
-                                if (GameViewFragment.textViewPlayer2Turn != null) {
-                                    GameViewFragment.textViewPlayer2Turn.post(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            GameViewFragment.textViewPlayer2Turn.setText(getResources().getString(R.string.textview_partnerturn));
-                                        }
-                                    });
-                                }
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    break;
-            }
-        }
-        invalidate();
-        return true;
+    public static GameView getGameView() {
+        return gameView;
     }
 
     public static void check() {
@@ -181,6 +89,121 @@ public class GameView extends View {
             GameViewFragment.textViewPlayer1Turn.setText("");
             GameViewFragment.textViewPlayer2Turn.setText("");
         }
-        line= i1 + ":" + j1 + ":" + i2 + ":" + j2;
+        line = i1 + ":" + j1 + ":" + i2 + ":" + j2;
+    }
+
+    public void InitGameView() {
+        getHolder().addCallback(this);
+        gameView = this;
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        startX = GRID_WIDTH / 2;
+        startY = GRID_WIDTH / 2;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+
+    public void draw() {
+        Canvas canvas = getHolder().lockCanvas();
+        canvas.drawColor(0xffe69310);
+        paint.setColor(Color.BLACK);//设置画笔为黑色
+        paint.setStrokeWidth(2.5f);//设置画笔宽度
+        for (int i = 0; i < GameConfig.getPOINT_NUM(); i++) {
+            canvas.drawLine(startX, startY + i * GRID_WIDTH, startX + GameConfig.getGRID_NUM()
+                    * GRID_WIDTH, startY + i * GRID_WIDTH, paint);//画横线
+            canvas.drawLine(startX + i * GRID_WIDTH, startY, startX + i
+                    * GRID_WIDTH, startY + GameConfig.getGRID_NUM() * GRID_WIDTH, paint);//画竖线
+        }
+        for (int i = 0; i < GameConfig.getPOINT_NUM(); i++) {
+            for (int j = 0; j < GameConfig.getPOINT_NUM(); j++) {
+                if (chess[i][j] == CHESS_BLACK) {
+                    paint.setColor(Color.BLACK);
+                    canvas.drawCircle(startX + i * GRID_WIDTH, startY + j * GRID_WIDTH, GRID_WIDTH / 2 - GRID_WIDTH * 0.05f, paint);
+                }
+                if (chess[i][j] == CHESS_WHITE) {
+                    paint.setColor(Color.WHITE);
+                    canvas.drawCircle(startX + i * GRID_WIDTH, startY + j * GRID_WIDTH, GRID_WIDTH / 2 - GRID_WIDTH * 0.05f, paint);
+                }
+            }
+        }
+        if (line != null) {
+            String[] separateLine = line.split(":", 4);
+            paint.setColor(0xffff0000);
+            canvas.drawLine(startX + Integer.parseInt(separateLine[0]) * GRID_WIDTH, startY + Integer.parseInt(separateLine[1]) * GRID_WIDTH, startX + Integer.parseInt(separateLine[2]) * GRID_WIDTH, startY + Integer.parseInt(separateLine[3]) * GRID_WIDTH, paint);
+        }
+        if (!gameEnd) {
+            if (lastx != null && lasty != null) {
+                paint.setColor(0xffff0000);
+                canvas.drawLine(startX + Integer.parseInt(lastx) * GRID_WIDTH - GRID_WIDTH * 0.2f, startY + Integer.parseInt(lasty) * GRID_WIDTH, startX + Integer.parseInt(lastx) * GRID_WIDTH + GRID_WIDTH * 0.2f, startY + Integer.parseInt(lasty) * GRID_WIDTH, paint);
+                canvas.drawLine(startX + Integer.parseInt(lastx) * GRID_WIDTH, startY + Integer.parseInt(lasty) * GRID_WIDTH - GRID_WIDTH * 0.2f, startX + Integer.parseInt(lastx) * GRID_WIDTH, startY + Integer.parseInt(lasty) * GRID_WIDTH + GRID_WIDTH * 0.2f, paint);
+            }
+        }
+        getHolder().unlockCanvasAndPost(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        if (((((touchX - startX) % GRID_WIDTH) < GRID_WIDTH * 0.4) || (((touchX - startX) % GRID_WIDTH) > GRID_WIDTH * 0.6))
+                && ((((touchY - startY) % GRID_WIDTH) < GRID_WIDTH * 0.4) || (((touchY - startY) % GRID_WIDTH) > GRID_WIDTH * 0.6))) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    int indexX = Math.round((touchX - startX) / GRID_WIDTH);
+                    int indexY = Math.round((touchY - startY) / GRID_WIDTH);
+                    if (turn && chess[indexX][indexY] == 0 && !gameEnd) {
+                        chess[indexX][indexY] = myColor;
+                        try {
+                            MyService.getMyService().out.writeUTF("play:place:" + GameConfig.getPartner() + ":" + indexX + ":" + indexY + ":" + myColor);
+                            lastx = String.valueOf(indexX);
+                            lasty = String.valueOf(indexY);
+                            turn = false;
+                            check();
+                            if (!GameView.gameEnd) {
+                                if (GameViewFragment.textViewPlayer1Turn != null) {
+                                    GameViewFragment.textViewPlayer1Turn.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            GameViewFragment.textViewPlayer1Turn.setText("");
+                                        }
+                                    });
+                                }
+                                if (GameViewFragment.textViewPlayer2Turn != null) {
+                                    GameViewFragment.textViewPlayer2Turn.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            GameViewFragment.textViewPlayer2Turn.setText(getResources().getString(R.string.textview_partnerturn));
+                                        }
+                                    });
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+            }
+        }
+        draw();
+        return true;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        draw();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
     }
 }
--- app/src/main/java/org/ivandgetic/fivechess/MyService.java
+++ app/src/main/java/org/ivandgetic/fivechess/MyService.java
@@ -21,21 +21,21 @@ import java.net.UnknownHostException;
 public class MyService extends Service {
     private static final int SOCKET_PORT = 50000;
     public static String SOCKET_ADDRESS = "192.168.1.210";//初始化
+    public static MyService myService;
     public Socket socket;
+    public GameViewFragment gameViewFragment;
     DataInputStream in;
     DataOutputStream out;
     SharedPreferences preferences;
-    public static MyService myService;
     String player1, player2;
-    public GameViewFragment gameViewFragment;
 
-
-    public static MyService getMyService() {
-        return myService;
-    }
 
     public MyService() {
         myService = this;
+    }
+
+    public static MyService getMyService() {
+        return myService;
     }
 
     @Override
@@ -78,7 +78,7 @@ public class MyService extends Service {
                                                 }
                                                 if (separate[1].equals("invitefrom")) {
                                                     GameConfig.setPartner(separate[2]);
-                                                    new AlertDialog.Builder(MyActivity.getMyActivity()).setTitle("Invite from: " + separate[2]).setPositiveButton(getString(R.string.button_agree), new DialogInterface.OnClickListener() {
+                                                    new AlertDialog.Builder(MyActivity.getMyActivity()).setTitle(getString(R.string.invite_from) + separate[2]).setPositiveButton(getString(R.string.button_agree), new DialogInterface.OnClickListener() {
                                                         @Override
                                                         public void onClick(DialogInterface dialogInterface, int i) {
                                                             try {
@@ -114,9 +114,9 @@ public class MyService extends Service {
                                             if (separate[0].equals("play")) {
                                                 if (separate[1].equals("place")) {
                                                     GameView.chess[Integer.parseInt(separate[2])][Integer.parseInt(separate[3])] = Integer.parseInt(separate[4]);
-                                                    GameView.lastx=separate[2];
-                                                    GameView.lasty=separate[3];
-                                                    GameView.getGameView().invalidate();
+                                                    GameView.lastx = separate[2];
+                                                    GameView.lasty = separate[3];
+                                                    GameView.getGameView().draw();
                                                     GameView.check();
                                                 }
                                                 if (separate[1].equals("turn")) {
